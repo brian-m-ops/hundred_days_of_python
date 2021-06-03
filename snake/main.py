ff6b5d87e676059de97e1b30805068db495f8983
brian-m-ops <bryzey@gmail.com>
2021-06-03 15:19:00 +1000

    Get the food onto the screen randomly

--- snake/main.py
+++ snake/main.py
@@ -1,5 +1,7 @@
 from turtle import Screen
 from snake import Snake
+from food import Food
+
 import time
 
 screen = Screen()
@@ -9,6 +11,7 @@ screen.title("Snake Game")
 screen.tracer(0)
 
 snake = Snake()
+food = Food()
 
 screen.listen()
 screen.onkey(snake.up, "Up")
