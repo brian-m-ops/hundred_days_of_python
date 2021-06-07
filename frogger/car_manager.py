8573bef9759a0ce83a9d623b21de5b47e3d2f561
brian-m-ops <bryzey@gmail.com>
2021-06-07 19:34:46 +1000

    Setup player class

--- frogger/car_manager.py
+++ frogger/car_manager.py
@@ -5,3 +5,9 @@ MOVE_INCREMENT = 10
 
 class CarManager:
     pass
+
+
+# Create cars that are 20px high by 40px wide that are randomly generated along the y-axis and move to the left edge
+# of the screen. No cars should be generated in the top and bottom 50px of the screen (think of it as a safe zone
+# for our little turtle). Hint: generate a new car only every 6th time the game loop runs. If you get stuck,
+# check the video walkthrough in Step 4.
--- frogger/main.py
+++ frogger/main.py
@@ -6,9 +6,27 @@ from scoreboard import Scoreboard
 
 screen = Screen()
 screen.setup(width=600, height=600)
+screen.title('Frogger')
 screen.tracer(0)
+
+player = Player()
+
+screen.listen()
+screen.onkey(player.go_up, "Up")
 
 game_is_on = True
 while game_is_on:
     time.sleep(0.1)
     screen.update()
+
+    # Detect when the turtle player collides with a car and stop
+    # the game if this happens. If you get stuck,
+    # check the video walkthrough in Step 5.
+
+
+    #Detect when the turtle player has reached the top edge of the screen
+    # (i.e., reached the FINISH_LINE_Y). When this happens, return the
+    # turtle to the starting position and increase the speed of the cars.
+    # Hint: think about creating an attribute and using the
+    # MOVE_INCREMENT to increase the car speed.
+    # If you get stuck, check the video walkthrough in Step 6.
